de55e1930d97d080510e3cdd52f950e0f545cd2b
elyahk <eldorbek.nusratov.98@gmail.com>
2022-08-21 21:59:42 +0500

    Add `CKTextField` components

--- ComponentKit/CKTextField/CKTextField.swift
+++ ComponentKit/CKTextField/CKTextField.swift
@@ -7,30 +7,53 @@ import Foundation
 import UIKit
 
 public class CKTextField: UIView {
+    private lazy var titleLabel: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.font = .systemFont(ofSize: 10.0, weight: .regular)
+        view.textColor = .secondaryLabel
+
+        return view
+    }()
+
     private lazy var textfield: UITextField = {
         let view = UITextField()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.font = .systemFont(ofSize: 17.0, weight: .regular)
+        view.textColor = .white
 
         return view
     }()
 
-    private lazy var textfield2: UITextField = {
-        let view = UITextField()
+    private lazy var errorLabel: UILabel = {
+        let view = UILabel()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.font = .systemFont(ofSize: 10.0, weight: .regular)
+        view.textColor = .systemRed
 
         return view
     }()
 
-    private lazy var textfield3: UITextField = {
-        let view = UITextField()
+    private lazy var contentStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [titleLabel, textfield, errorLabel])
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
 
         return view
     }()
 
-    private lazy var textfield4: UITextField = {
-        let view = UITextField()
+    private lazy var contentView: UIView = {
+        let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .darkGray
+
+        view.addSubview(contentStackView)
+        NSLayoutConstraint.activate([
+            contentStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 6.0),
+            contentStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16.0),
+            contentStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16.0),
+            contentStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -6.0)
+        ])
 
         return view
     }()
@@ -49,6 +72,13 @@ public class CKTextField: UIView {
 
 extension CKTextField {
     private func setupSubviews() {
+        self.addSubview(contentView)
 
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: self.topAnchor),
+            contentView.leftAnchor.constraint(equalTo: self.leftAnchor),
+            contentView.rightAnchor.constraint(equalTo: self.rightAnchor),
+            contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
     }
 }
