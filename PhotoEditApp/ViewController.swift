26d038ca39d51b036d0336531d8c5b27ad139bbd
elyahk <eldorbek.nusratov.98@gmail.com>
2022-08-21 08:40:11 +0500

    Initial Commit

---
+++ PhotoEditApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoEditApp
+//
+//  Created by eldorbek nusratov on 21/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
