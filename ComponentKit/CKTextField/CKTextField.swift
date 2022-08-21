49211332d64f7ccc6d0cdd1e429ef7b5fdb9e064
elyahk <eldorbek.nusratov.98@gmail.com>
2022-08-21 21:41:39 +0500

    Add temporary `CKTextField`

--- ComponentKit/CKTextField/CKTextField.swift
+++ ComponentKit/CKTextField/CKTextField.swift
@@ -6,4 +6,49 @@
 import Foundation
 import UIKit
 
-public class CKTextField: UIView {}
+public class CKTextField: UIView {
+    private lazy var textfield: UITextField = {
+        let view = UITextField()
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    private lazy var textfield2: UITextField = {
+        let view = UITextField()
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    private lazy var textfield3: UITextField = {
+        let view = UITextField()
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    private lazy var textfield4: UITextField = {
+        let view = UITextField()
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Layout
+
+extension CKTextField {
+    private func setupSubviews() {
+
+    }
+}
