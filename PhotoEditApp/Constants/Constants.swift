76b0f305a3d18fdf4099a05d571e99806d436bd7
Ogabek <ogabekjan07@gmail.com>
2022-08-21 22:14:48 +0500

    Add Sign in page colors to Constants file.

--- PhotoEditApp/Constants/Constants.swift
+++ PhotoEditApp/Constants/Constants.swift
@@ -6,13 +6,26 @@
 //
 
 import Foundation
+import UIKit
 
 enum Images{
     
 }
 
 enum Colors{
+    case baseBackgroundDark
+    case basePurple
+    case baseTextFieldDarkGray
+    case baseTextWhiteColor
     
+    var colors: UIColor?{
+        switch self {
+        case .baseBackgroundDark: return UIColor(named: "baseBackgroundDark")
+        case .basePurple: return UIColor(named: "basePurple")
+        case .baseTextFieldDarkGray: return UIColor(named: "baseTextFieldDarkGray")
+        case .baseTextWhiteColor: return .white
+        }
+    }
 }
 
 enum Strings{
