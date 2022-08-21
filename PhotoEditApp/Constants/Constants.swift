07f9532506d9f28b080cfc5d89a4b4b012b07224
Ogabek <ogabekjan07@gmail.com>
2022-08-21 21:01:38 +0500

    Add Constants

---
+++ PhotoEditApp/Constants/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  PhotoEditApp
+//
+//  Created by Ogabek Bakhodirov on 21/08/22.
+//
+
+import Foundation
+
+enum Images{
+    
+}
+
+enum Colors{
+    
+}
+
+enum Strings{
+    
+}
