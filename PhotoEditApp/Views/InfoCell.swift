2fb046eb8a22638191647fa84031c954d8bf481a
SardorK <sardorkarshiev6167@gmail.com>
2022-08-29 23:39:32 +0500

    Change the InfoCell add shadow with Cornerradius

--- PhotoEditApp/Views/InfoCell.swift
+++ PhotoEditApp/Views/InfoCell.swift
@@ -8,15 +8,15 @@ import SnapKit
 import ComponentKit
 
 class InfoCell: UICollectionViewCell {
-    lazy var backView: UIView = {
-        let view = UIView()
+    lazy var backView: CustomView = {
+        let view = CustomView()
         view.backgroundColor = Colors.base_background_dark.color
 //        view.clipsToBounds = true
 //        view.layer.cornerRadius = 10.0
-        view.layer.shadowColor = UIColor.white.cgColor
-        view.layer.shadowOpacity = 1.0
-        view.layer.shadowOffset = CGSize.zero
-        view.layer.shadowRadius = 2.0
+//        view.layer.shadowColor = UIColor.white.cgColor
+//        view.layer.shadowOpacity = 1.0
+//        view.layer.shadowOffset = CGSize.zero
+//        view.layer.shadowRadius = 2.0
 
         return view
     }()
@@ -55,6 +55,29 @@ class InfoCell: UICollectionViewCell {
             make.left.equalTo(backView.snp_leftMargin).inset(1.0)
             make.top.equalTo(backView.snp_topMargin).inset(1.0)
             make.bottom.equalTo(backView.snp_bottomMargin).inset(-1.0)
+        }
+    }
+}
+
+final class CustomView: UIView {
+    private var shadowLayer: CAShapeLayer!
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        if shadowLayer == nil {
+            shadowLayer = CAShapeLayer()
+            shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: 8.0).cgPath
+            shadowLayer.fillColor = Colors.base_background_dark.color?.cgColor
+
+            shadowLayer.shadowColor = UIColor.white.cgColor
+            shadowLayer.shadowPath = shadowLayer.path
+            shadowLayer.shadowOffset = CGSize(width: 0.3, height: 0.3)
+            shadowLayer.shadowOpacity = 0.3
+            shadowLayer.shadowRadius = 2
+
+            layer.insertSublayer(shadowLayer, at: 0)
+            // layer.insertSublayer(shadowLayer, below: nil) // also works
         }
     }
 }
